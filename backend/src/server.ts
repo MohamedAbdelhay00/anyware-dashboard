031d4a442f367ec2d4707af2f7db4ca89a5c173e
MohamedAbdelhay00 <mohamed.a.abdelhay55@gmail.com>
2025-05-17 18:28:41 +0300

    Build fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,9 @@ connectDB();
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://anyware-task-j7sl.vercel.app'],
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
