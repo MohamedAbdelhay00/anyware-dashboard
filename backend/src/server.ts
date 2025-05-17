c840a23c4bfb537c32ae5e217bed0c46d8b76166
MohamedAbdelhay00 <mohamed.a.abdelhay55@gmail.com>
2025-05-17 19:08:37 +0300

    Build fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://anyware-task-j7sl.vercel.app',
+    'https://anyware-task-kappa.vercel.app/',
   ],
   credentials: true,
 }));
