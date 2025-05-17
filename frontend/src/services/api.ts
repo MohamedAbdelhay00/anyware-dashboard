f8fa04f69a949f33c81cc56c5a38991b6cb3117b
MohamedAbdelhay00 <mohamed.a.abdelhay55@gmail.com>
2025-05-17 19:06:49 +0300

    Build fixes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://delightful-wholeness-production.up.railway.app'
+const API_URL = 'https://delightful-wholeness-production.up.railway.app/api'
 
 const api = axios.create({
   baseURL: API_URL,
